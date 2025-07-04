3d4a6c809d0d7bd568edcce25f5834f996dbc71f
Võ Minh Tuấn <21127472@student.hcmus.edu.vn>
2025-07-04 16:31:30 +0700

    add Vuln component of ISSUES page

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -6,20 +6,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import Dashboard from './pages/Dashboard.tsx'
 import Assets from './pages/Assets.tsx'
+import VulnerabilityTable from './components/Issues/VulnerabilityTable.tsx'
+//import Issues from './pages/Issues.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+{/* 
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/assets" element={<Assets />} />
+        <Route path="/issues" element={<Issues />} />
 
       </Routes>
       
 
-    </BrowserRouter>
-
-  </StrictMode>,
+    </BrowserRouter> */}
+    <VulnerabilityTable vulnerabilities={[]} onUpdateStatus={function (id: string, isFixed: boolean): void {
+      throw new Error('Function not implemented.')
+    } } isLoading={false} />
+    
+  </StrictMode>
 )
 
