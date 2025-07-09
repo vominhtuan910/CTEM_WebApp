16df1bb23d0d6e0edd0c99fe768a9f148b0183de
Võ Minh Tuấn <21127472@student.hcmus.edu.vn>
2025-07-09 12:33:58 +0700

    delete backend folder

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -5,14 +5,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
 
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    
+
   </StrictMode>
 )
 
